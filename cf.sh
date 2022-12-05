8e20c52b7c950ef4f9a15b2bf1a8fb52f69e9f72
jhoker <jhokerhacker@gmail.com>
2022-12-06 04:05:57 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -37,5 +37,9 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 echo "Host : $SUB_DOMAIN"
+echo $SUB_DOMAIN > /root/scdomain
+echo $SUB_DOMAIN > /etc/xray/scdomain
+echo $SUB_DOMAIN > /etc/xray/domain
+echo $SUB_DOMAIN > /etc/v2ray/domain
 echo $SUB_DOMAIN > /root/domain
 rm -f /root/cf.sh
